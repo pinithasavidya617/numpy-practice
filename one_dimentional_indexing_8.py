6d7a7436dd7f18902881295c1d2da704b096e9d8
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-04-10 12:48:02 +0530

    Add slicing for 1D arrays

--- one_dimentional_indexing_8.py
+++ one_dimentional_indexing_8.py
@@ -7,3 +7,9 @@ a[-1]     # 50     (last element)
 a[1:4]    # [20, 30, 40]  (slice)
 a[::2]    # [10, 30, 50]  (every other element)
 a[::-1]   # [50, 40, 30, 20, 10]  (reversed)
+
+b = np.array([5, 10, 15, 20, 25, 30])
+
+print(b[1:5])
+print(b[::3])
+print(b[::-1])
